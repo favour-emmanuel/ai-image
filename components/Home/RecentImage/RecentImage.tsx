b85daf2f1558408f6bfa4997e5ea589ed3a1c945
Favour Emmanuel <iyenefavour@gmail.com>
2025-03-04 04:58:50 +0100

    feat: created a sectionHeader component and Image slider component install: react multi carousel

--- components/Home/RecentImage/RecentImage.tsx
+++ components/Home/RecentImage/RecentImage.tsx
@@ -1,5 +1,17 @@
+import SectionHeading from "@/components/Helper/SectionHeading";
+import ImageSlider from "./ImageSlider";
+
 const RecentImage = () => {
-  return <div>RecentImage</div>;
+  return (
+    <div className="pt-16 pb-16 bg-[#000614]">
+      {/* heading */}
+      <SectionHeading heading="Explore Our Recent Image" />
+      {/* slider */}
+      <div className="w-[90%] md:w-[80%] mx-auto mt-16">
+        <ImageSlider />
+      </div>
+    </div>
+  );
 };
 
 export default RecentImage;
